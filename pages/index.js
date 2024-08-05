5415c8719b46e46a6974a602b6677b756ea74374
med-wf <med@med.wf>
2024-08-05 02:19:20 +0200

    Add Vercel Analytics and Speed-Insights
    Signed-off-by: med-wf <med@med.wf>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import WebLinks from '../components/WebLinks';
 import Seo from '../components/Seo';
 import seoData from '../next-seo.config';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export default function Home() {
@@ -14,6 +16,8 @@ export default function Home() {
     <>
       <Seo page={page} />
       <WebLinks />
+      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
